f80c1fc3dd92b908a101168ecea92f7fb355aa6b
Zhenghao Xu <zhenghaoxu@gatech.edu>
2024-02-26 16:37:39 -0500

    rescale

--- loralib/layers.py
+++ loralib/layers.py
@@ -146,7 +146,8 @@ class Linear(nn.Linear, LoRALayer):
                     Phi = torch.randn_like(self.lora_B) / math.sqrt(self.r)
                     sketched_weight = self.weight.data @ Phi
                     U, S, V = torch.linalg.svd(sketched_weight)
-                self.lora_B.data.copy_(U[:,:self.r])
+                # self.lora_B.data.copy_(U[:,:self.r])
+                self.lora_B.data = U[:,:self.r] / math.sqrt(self.r)
                 # print('<<<<<', self.lora_B.norm(), self.lora_B.requires_grad)
                 # nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
                 # print(self.lora_A.norm(), self.lora_A.requires_grad, '>>>>>')
