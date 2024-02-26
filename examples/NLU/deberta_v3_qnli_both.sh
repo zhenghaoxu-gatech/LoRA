04a1c462acd719d33ce3973869692a14062e98e9
Zhenghao Xu <zhenghaoxu@gatech.edu>
2024-02-26 02:03:15 -0500

    smaller batch

--- examples/NLU/deberta_v3_qnli_both.sh
+++ examples/NLU/deberta_v3_qnli_both.sh
@@ -10,8 +10,8 @@ examples/text-classification/run_glue.py \
 --do_train \
 --do_eval \
 --max_seq_length 512 \
---per_device_train_batch_size 32 \
---per_device_eval_batch_size 32 \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
 --learning_rate 1e-4 \
 --num_train_epochs 8 \
 --output_dir $output_dir_col/model \
@@ -40,8 +40,8 @@ examples/text-classification/run_glue.py \
 --do_train \
 --do_eval \
 --max_seq_length 512 \
---per_device_train_batch_size 32 \
---per_device_eval_batch_size 32 \
+--per_device_train_batch_size 16 \
+--per_device_eval_batch_size 16 \
 --learning_rate 1e-4 \
 --num_train_epochs 8 \
 --output_dir $output_dir/model \
